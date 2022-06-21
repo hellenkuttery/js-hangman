3f7a89ff2a3746221a6d155dce3b6dfc6a156cb7
hellenkuttery <helen@clarusway.com>
2022-06-21 13:15:39 +0300

    add correct letters

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
 const myWord=document.getElementById("word");
-const correctLetter=[s,a];
+const popup=document.getElementById("popup-container");
+const myMessage=document.getElementById("message")
+const correctLetters=['t','a','j','c','v','s','p','r','i'];
 const worngLetters=[];
 
+
 function RandomWord(){
-    const words=["javascript","scratch","html","css"];
+    const words=["javascript","scratch","html","java"];
     return words[Math.floor(Math.random()*words.length)];
 }
-
+const selectedWord=RandomWord();
 function display(){
-    const selectedWord=RandomWord();
+    
     console.log(selectedWord);
 
     myWord.innerHTML=`${selectedWord.split("").map(letter=>`
         <div class="letter">
-            ${letter}
+            ${correctLetters.includes(letter) ? letter : "" }
         </div>
-        `).join("")}`
+        `).join("")}`;
+
+ const findWord=myWord.innerText.replace(/\n/g,'')
+    console.log(findWord)
+       if (selectedWord===findWord){
+        popup.style.display="flex";
+        myMessage.innerText="Tebrikler Kazandınız"
+       }
 }
+
+window.addEventListener("keydown",function(e){
+    // Basılan keycode görme için olan 
+    // console.log(e.key);
+    // console.log(e.keyCode)
+
+    if (e.keyCode >=65 && e.keyCode <=90 ){
+       const letter=e.key;
+
+       if (selectedWord.includes(letter)){
+        if ( correctLetters.includes(letter)){
+            correctLetters.push(letter);
+            display();
+        }
+        else{
+          console.log("Bu harfi zaten ekledin")
+        }
+       
+        } else {
+            if (!worngLetters.includes(letter)){
+                worngLetters.push(letter)
+                console.log("Hatalı harfler",e.key)
+            }
+       }
+    }
+
+    
+})
 
 display()
